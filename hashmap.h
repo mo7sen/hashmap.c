e6facd49f696f4e9e8dcc5dc8d790c7d933ca09f
Robear Selwans <robear.selwans@outlook.com>
2021-04-26 10:27:42 +0000

    Multiple changes
    * Refactored multiple macros
    * Added hashmap entry destructors
    * Added overrides for the HashmapDefine macro
    
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- hashmap.h
+++ hashmap.h
@@ -53,11 +53,11 @@ HM_API uint64_t hashmap_sip(const void *data, size_t len,
 HM_API uint64_t hashmap_murmur(const void *data, size_t len, 
                         uint64_t seed0, uint64_t seed1);
 
-#define HM_CONCAT_IMPL(a,b) a##b
-#define HM_CONCAT(a,b) HM_CONCAT_IMPL(a,b)
+#define CAT2(a,b) a##b
+#define CAT(a,b) CAT2(a,b)
 
-#define HashFunction(K) HM_CONCAT(HashFunction_,K)
-#define CmpFunction(K) HM_CONCAT(CmpFunction_,K)
+#define HashFunction(K) CAT(HashFunction_,K)
+#define CmpFunction(K)  CAT(CmpFunction_,K)
 
 #define HashFunctionDefineCustom(K, pVar) \
   static uint64_t                         \
@@ -90,120 +90,166 @@ HM_API uint64_t hashmap_murmur(const void *data, size_t len,
   }
 
 #define Map(K, V) struct hashmap *
-#define HashmapEntry(K, V) struct { K key; V value; }
 
-#define Hashmap(K, V)     HM_CONCAT(HM_CONCAT(HashmapStruct_, K), HM_CONCAT(_, V))
-#define HashmapNew(K, V)  HM_CONCAT(HM_CONCAT(HashmapNew_, K), HM_CONCAT(_, V))
-#define HashmapPush(K, V) HM_CONCAT(HM_CONCAT(HashmapPush_, K), HM_CONCAT(_, V))
-#define HashmapGet(K, V)  HM_CONCAT(HM_CONCAT(HashmapGet_, K), HM_CONCAT(_, V))
-#define HashmapSize(K, V) HM_CONCAT(HM_CONCAT(HashmapSize_, K), HM_CONCAT(_, V))
-#define HashmapFree(K, V) HM_CONCAT(HM_CONCAT(HashmapFree_, K), HM_CONCAT(_, V))
-#define HashmapRemove(K, V) HM_CONCAT(HM_CONCAT(HashmapRemove_, K), HM_CONCAT(_, V))
-#define HashmapClear(K, V) HM_CONCAT(HM_CONCAT(HashmapClear_, K), HM_CONCAT(_, V))
-#define HashmapIter(K, V) HM_CONCAT(HM_CONCAT(HashmapIter_, K), HM_CONCAT(_, V))
-#define HashmapIterImpl(K, V) HM_CONCAT(HM_CONCAT(HashmapIterImpl_, K), HM_CONCAT(_, V))
+#define Hashmap(K, V) CAT(CAT(CAT(HashmapStruct_, K), _), V)
 
-#define HashmapNewDefine(K, V)        \
-  static Map(K, V) HashmapNew(K, V)() \
-  {                                   \
-    return hashmap_new(               \
-        sizeof(K) + sizeof(V),        \
-        0, 0, 0,                      \
-        (hm_hash_fn)HashFunction(K),  \
-        (hm_cmp_fn)CmpFunction(K),    \
-        NULL);                        \
+#define HashmapEntry(K, V) CAT(CAT(CAT(HashmapEntryStruct_, K), _), V)
+#define HashmapEntryDefine(K, V) typedef struct { K key; V value; } HashmapEntry(K,V);
+
+#define HashmapMethod(K,V,Name)   CAT(CAT(CAT(CAT(CAT(CAT(Hashmap,Name),_),K),_),V),_Method)
+#define HashmapFunction(K,V,Name) CAT(CAT(CAT(CAT(CAT(CAT(Hashmap,Name),_),K),_),V),_Function)
+#define HashmapDestructor(K,V)    CAT(CAT(CAT(CAT(CAT(CAT(Hashmap,Name),_),K),_),V),_Destructor)
+
+#define HashmapNewDefine(K, V)                \
+  static Map(K, V) HashmapMethod(K, V, New)() \
+  {                                           \
+    return hashmap_new(                       \
+        sizeof(K) + sizeof(V),                \
+        0, 0, 0,                              \
+        (hm_hash_fn)HashFunction(K),          \
+        (hm_cmp_fn)CmpFunction(K),            \
+        NULL);                                \
   }
 
-#define HashmapPushDefine(K, V)                              \
-  static void HashmapPush(K, V)(Map(K, V) map, K key, V val) \
-  {                                                          \
-    HashmapEntry(K, V) entry = {key, val};                   \
-    hashmap_set(map, &entry);                                \
-  }
-
-#define HashmapGetDefine(K, V)                          \
-  static V* HashmapGet(K, V)(Map(K, V) map, K key)      \
-  {                                                     \
-    HashmapEntry(K, V) entry = {.key = key};            \
-    HashmapEntry(K, V) *res = hashmap_get(map, &entry); \
-    if(res) {                                           \
-      return &(res->value);                             \
-    }                                                   \
-    return NULL;                                        \
-  }
-
-#define HashmapRemoveDefine(K, V)                       \
-  static void HashmapRemove(K, V)(Map(K, V) map, K key) \
-  {                                                     \
-    HashmapEntry(K, V) entry = {.key = key};            \
-    hashmap_delete(map, &entry);                        \
-  }
-
-#define HashmapSizeDefine(K, V)                  \
-  static size_t HashmapSize(K, V)(Map(K, V) map) \
-  {                                              \
-    return hashmap_count(map);                   \
-  }
-
-#define HashmapFreeDefine(K, V)                \
-  static void HashmapFree(K, V)(Map(K, V) map) \
-  {                                            \
-    hashmap_free(map);                         \
-  }
-
-#define HashmapClearDefine(K, V)                \
-  static void HashmapClear(K, V)(Map(K, V) map) \
-  {                                             \
-    hashmap_clear(map, false);                  \
-  }
-
-#define HashmapIterImplDefine(K, V)                                  \
-  static bool HashmapIterImpl(K, V)(const void *data, void* iter_fn) \
+#define HashmapPushDefine(K, V)                                      \
+  static void HashmapMethod(K, V, Push)(Map(K, V) map, K key, V val) \
   {                                                                  \
-    const HashmapEntry(K, V) *entry = data;                          \
-    ((void(*)(K,V))iter_fn)(entry->key, entry->value);               \
-    return true;                                                     \
+    HashmapEntry(K, V) entry = {key, val};                           \
+    HashmapEntry(K, V) *prev = hashmap_set(map, &entry);       \
+    if(prev) {                                                       \
+      HashmapDestructor(K,V)(prev);                                  \
+    }                                                                \
   }
 
-#define HashmapIterDefine(K, V)                                     \
-  static void HashmapIter(K, V)(Map(K, V) map, void(*iter_fn)(K,V)) \
-  {                                                                 \
-    hashmap_scan(map, HashmapIterImpl(K, V), iter_fn);              \
-  }                                                                 \
+#define HashmapGetDefine(K, V)                             \
+  static V* HashmapMethod(K, V, Get)(Map(K, V) map, K key) \
+  {                                                        \
+    HashmapEntry(K, V) entry = {.key = key};               \
+    HashmapEntry(K, V) *res = hashmap_get(map, &entry);    \
+    if(res) {                                              \
+      return &(res->value);                                \
+    }                                                      \
+    return NULL;                                           \
+  }
 
-#define HashmapDefine(K, V)                     \
-  HashmapNewDefine(K, V)                        \
-  HashmapPushDefine(K, V)                       \
-  HashmapGetDefine(K, V)                        \
-  HashmapRemoveDefine(K, V)                     \
-  HashmapSizeDefine(K, V)                       \
-  HashmapFreeDefine(K, V)                       \
-  HashmapClearDefine(K, V)                      \
-  HashmapIterImplDefine(K, V)                   \
-  HashmapIterDefine(K, V)                       \
-  static struct {                               \
-    uint32_t key_size;                          \
-    uint32_t val_size;                          \
-    Map(K, V)(*new)();                          \
-    void(*push)(Map(K,V),K,V);                  \
-    V*(*get)(Map(K,V),K);                       \
-    void(*remove)(Map(K,V),K);                  \
-    size_t(*size)(Map(K,V));                    \
-    void(*free)(Map(K,V));                      \
-    void(*clear)(Map(K,V));                     \
-    void(*iter)(Map(K,V), void(*iter_fn)(K,V)); \
-  } Hashmap(K, V) = {                           \
-    .key_size = sizeof(K),                      \
-    .val_size = sizeof(V),                      \
-    .new = HashmapNew(K,V),                     \
-    .push = HashmapPush(K,V),                   \
-    .get = HashmapGet(K,V),                     \
-    .remove = HashmapRemove(K,V),               \
-    .size = HashmapSize(K,V),                   \
-    .free = HashmapFree(K,V),                   \
-    .clear = HashmapClear(K,V),                 \
-    .iter = HashmapIter(K,V),                   \
+#define HashmapRemoveDefine(K, V)                               \
+  static void HashmapMethod(K, V, Remove)(Map(K, V) map, K key) \
+  {                                                             \
+    HashmapEntry(K, V) entry = {.key = key};                    \
+    HashmapEntry(K, V) *old = hashmap_delete(map, &entry);      \
+    if(old) {                                                   \
+      HashmapDestructor(K, V)(old);                             \
+    }                                                           \
+  }
+
+#define HashmapSizeDefine(K, V)                          \
+  static size_t HashmapMethod(K, V, Size)(Map(K, V) map) \
+  {                                                      \
+    return hashmap_count(map);                           \
+  }
+
+#define HashmapFreeDefine(K, V)                        \
+  static void HashmapMethod(K, V, Free)(Map(K, V) map) \
+  {                                                    \
+    HashmapMethod(K, V, Clear)(map);                   \
+    hashmap_free(map);                                 \
+  }
+
+#define HashmapClearDefine(K, V)                         \
+  static bool destroyEach(const void *data, void *udata) \
+  {                                                      \
+    HashmapDestructor(K, V)(data);                       \
+    return true;                                         \
+  }                                                      \
+  static void HashmapMethod(K, V, Clear)(Map(K, V) map)  \
+  {                                                      \
+    hashmap_scan(map, destroyEach, NULL);                \
+    hashmap_clear(map, false);                           \
+  }
+
+#define HashmapIterImplDefine(K, V)                                            \
+  static bool HashmapFunction(K, V, IterImpl)(const void *data, void* iter_fn) \
+  {                                                                            \
+    const HashmapEntry(K, V) *entry = data;                                    \
+    ((void(*)(K,V))iter_fn)(entry->key, entry->value);                         \
+    return true;                                                               \
+  }
+
+#define HashmapIterDefine(K, V)                                             \
+  static void HashmapMethod(K, V, Iter)(Map(K, V) map, void(*iter_fn)(K,V)) \
+  {                                                                         \
+    hashmap_scan(map, HashmapFunction(K, V, IterImpl), iter_fn);            \
+  }                                                                         \
+
+#define HashmapDestructorDefine(K, V, K_destr, V_destr)                      \
+  static inline void HashmapDestructor(K, V)(const HashmapEntry(K,V) *entry) \
+  {                                                                          \
+    if(K_destr) ((void(*)(K))K_destr)(entry->key);                           \
+    if(V_destr) ((void(*)(V))V_destr)(entry->value);                         \
+  }
+
+// Both are the same for now, might need to change?
+#define HashmapMethodDefine(K, V, Name)   CAT(CAT(Hashmap, Name), Define)(K, V)
+#define HashmapFunctionDefine(K, V, Name) CAT(CAT(Hashmap, Name), Define)(K, V)
+
+#define VARGS_NARG(...) VARGS_NARG_IMPL(__VA_ARGS__, VARGS_RSEQ_N())
+#define VARGS_NARG_IMPL(...) VARGS_ARG_N(__VA_ARGS__)
+#define VARGS_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, N, ...) N
+#define VARGS_RSEQ_N() 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+
+#define HashmapDefine(...) CAT(__HashmapDefine_IMPL_,VARGS_NARG(__VA_ARGS__))(__VA_ARGS__)
+
+#define __HashmapDefine_IMPL_1(x) static_assert("Can't define a hashmap with a single type. Need (Key,Val) typepair");
+#define __HashmapDefine_IMPL_2(K, V) __HashmapDefine_IMPL_4(K, V, NULL, NULL)
+#define __HashmapDefine_IMPL_3(K, V, destr) __HashmapDefine_IMPL_4(K, V, destr, destr)
+
+#define __HashmapDefine_IMPL_4(K, V, K_destr, V_destr)     \
+  HashmapEntryDefine(K, V)                        \
+  HashmapDestructorDefine(K, V, K_destr, V_destr) \
+  HashmapMethodDefine(K, V, New)                  \
+  HashmapMethodDefine(K, V, Push)                 \
+  HashmapMethodDefine(K, V, Get)                  \
+  HashmapMethodDefine(K, V, Remove)               \
+  HashmapMethodDefine(K, V, Size)                 \
+  HashmapMethodDefine(K, V, Clear)                \
+  HashmapMethodDefine(K, V, Free)                 \
+  HashmapFunctionDefine(K, V, IterImpl)           \
+  HashmapMethodDefine(K, V, Iter)                 \
+  static struct {                                 \
+    uint32_t key_size;                            \
+    uint32_t val_size;                            \
+    Map(K, V)(*new)();                            \
+    void(*push)(Map(K,V),K,V);                    \
+    V*(*get)(Map(K,V),K);                         \
+    void(*remove)(Map(K,V),K);                    \
+    size_t(*size)(Map(K,V));                      \
+    void(*free)(Map(K,V));                        \
+    void(*clear)(Map(K,V));                       \
+    void(*iter)(Map(K,V), void(*iter_fn)(K,V));   \
+  } Hashmap(K, V) = {                             \
+    .key_size  = sizeof(K),                       \
+    .val_size  = sizeof(V),                       \
+    .new       = HashmapMethod(K,V,New),          \
+    .push      = HashmapMethod(K,V,Push),         \
+    .get       = HashmapMethod(K,V,Get),          \
+    .remove    = HashmapMethod(K,V,Remove),       \
+    .size      = HashmapMethod(K,V,Size),         \
+    .free      = HashmapMethod(K,V,Free),         \
+    .clear     = HashmapMethod(K,V,Clear),        \
+    .iter      = HashmapMethod(K,V,Iter),         \
   };
+
+#define __HashmapDefine_IMPL_5(...)  static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_6(...)  static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_7(...)  static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_8(...)  static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_9(...)  static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_10(...) static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_11(...) static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_12(...) static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_13(...) static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_14(...) static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
+#define __HashmapDefine_IMPL_15(...) static_assert("Passed too many arguments to HashmapDefine. Accepted argument counts are (2, 3, 4).");
 
 
 #endif
